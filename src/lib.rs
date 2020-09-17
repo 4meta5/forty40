fb534f804564bb2441846554998df91b1bb07394
4meta5 <asinghchrony@protonmail.com>
2020-09-17 17:19:28 -0400

    rm lex

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,2 @@
 mod math;
 mod shuffle;
-pub use shuffle::heap::Heap;
-pub use shuffle::lex::Lex;
--- src/shuffle/mod.rs
+++ src/shuffle/mod.rs
@@ -1,4 +1,3 @@
 //! Functions for generating permutations
 //! -> future goals, searching permutations for subsequences
 pub mod heap;
-pub mod lex;
